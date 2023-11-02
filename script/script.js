cc259bb37ecc46e1c4a0ba43a9bf3c140959e2c0
Kevin Haro <artcom_3@hotmail.com>
2023-11-01 22:49:30 -0600

    Add home slider

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide-article");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1].style.display = "flex";  
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
